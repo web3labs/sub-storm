1955472ee976368d88f02c596dbb0afac13eea7c
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2022-06-15 10:01:12 +0530

    spawn 4 tasks for balance xfer

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use subxt::{
     PairSigner,
     PolkadotExtrinsicParams,
 };
+use std::sync::Arc;
 
 #[subxt::subxt(runtime_metadata_path = "polkadot_metadata.scale")]
 pub mod polkadot {}
@@ -13,32 +14,37 @@ pub mod polkadot {}
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
-    let signer = PairSigner::new(AccountKeyring::Alice.pair());
+    let signer = Arc::new(PairSigner::new(AccountKeyring::Alice.pair()));
 
     let api = ClientBuilder::new()
         .set_url("ws://127.0.0.1:9944")
         .build()
         .await?
         .to_runtime_api::<polkadot::RuntimeApi<DefaultConfig, PolkadotExtrinsicParams<DefaultConfig>>>();
+    let api = Arc::new(api);
 
-    tokio::spawn(async move {
-        let dest = AccountKeyring::Bob.to_account_id().into();
+    for _ in 0..5 {
+        let api = api.clone();
+        let signer = signer.clone();
+        tokio::spawn(async move {
+            let dest = AccountKeyring::Bob.to_account_id().into();
 
-        // Obtain an extrinsic, calling the "transfer" function in
-        // the "balances" pallet.
-        let extrinsic = match api.tx().balances().transfer(dest, 123_456_789_012_345) {
-            Ok(extrinsic) => extrinsic,
-            Err(_) => { return }
-        };
+            // Obtain an extrinsic, calling the "transfer" function in
+            // the "balances" pallet.
+            let extrinsic = match api.tx().balances().transfer(dest, 1) {
+                Ok(extrinsic) => extrinsic,
+                Err(_) => { return }
+            };
 
-        // Sign and submit the extrinsic, returning its hash.
-        let tx_hash = match extrinsic.sign_and_submit_default(&signer).await {
-            Ok(tx_hash) => tx_hash,
-            Err(_) => { return }
-        };
+            // Sign and submit the extrinsic, returning its hash.
+            let tx_hash = match extrinsic.sign_and_submit_default(&*signer).await {
+                Ok(tx_hash) => tx_hash,
+                Err(_) => { return }
+            };
 
-        println!("Balance transfer extrinsic submitted: {}", tx_hash);
-    }).await?;
+            println!("Balance transfer extrinsic submitted: {}", tx_hash);
+        }).await?;
+    }
 
     Ok(())
 }
